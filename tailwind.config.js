63fe66647b56f212a25f13994cf6c4f56e14db9f
Ansh Goyal <anshg1214@gmail.com>
2022-08-27 15:59:49 +0530

    feat: Setup and initial configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,ts,jsx,tsx}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				'primary-blue': '#79C5E7',
+				'secondary-blue': '#1D4ED8'
+			}
+		}
+	},
+	plugins: []
+};
